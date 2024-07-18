10d57d1cfdc71a6ef504bf8808e1b3a2b2c54386
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-07-18 21:24:13 +0800

    refactored good practice.

--- highway-apis/main.go
+++ highway-apis/main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"highway-apis/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	l := log.New(os.Stdout, "product-apis", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	// create handler
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello, World!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Println("Data : ", string(d))
-	})
-	// another Handler with Path
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye, World!")
-	})
 	// create server
-	http.ListenAndServe(":8080", nil)
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Println("Received terminate, gracefully shutting down", sig)
+
+	tx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tx)
 
 }
